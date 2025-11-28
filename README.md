f1a32d38e9549248371e1d373d02aae0b03f0845
Matthew James <matthew.stephen.james@gmail.com>
2025-11-28 18:15:08 +0800

    self hosted... again

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Github Self-hosted Runner with Docker
 
-1. Set `REPO` and `TOKEN` in `.env` (repo is user/reponame and token is from github action settings -> create new self-hosted runner)
-2. `docker compose up -d`
+1. Set `REPO` to `user/projectname` in `.env`
+2. Create a personal access token with Administrative write-access on the repo and set `TOKEN` to this value in `.env`
+3. `docker compose up -d`
--- start.sh
+++ start.sh
@@ -6,16 +6,20 @@ ACCESS_TOKEN=$TOKEN
 echo "REPO ${REPOSITORY}"
 echo "ACCESS_TOKEN ${ACCESS_TOKEN}"
 
-# REG_TOKEN=$(curl -X POST -H "Authorization: token ${ACCESS_TOKEN}" -H "Accept: application/vnd.github+json" https://api.github.com/repos/${REPOSITORY}/actions/runners/registration-token | jq .token --raw-output)
+curl -X POST -H "Authorization: token ${ACCESS_TOKEN}" -H "Accept: application/vnd.github+json" https://api.github.com/repos/${REPOSITORY}/actions/runners/registration-token
+
+REG_TOKEN=$(curl -X POST -H "Authorization: token ${ACCESS_TOKEN}" -H "Accept: application/vnd.github+json" https://api.github.com/repos/${REPOSITORY}/actions/runners/registration-token | jq .token --raw-output)
+
+echo "REG_TOKEN ${REG_TOKEN}"
 
 cd /home/docker/actions-runner
 
-./config.sh remove --unattended --token ${ACCESS_TOKEN}
-./config.sh --url https://github.com/${REPOSITORY} --token ${ACCESS_TOKEN}
+./config.sh remove --token ${ACCESS_TOKEN}
+./config.sh --url https://github.com/${REPOSITORY} --token ${REG_TOKEN}
 
 cleanup() {
     echo "Removing runner..."
-    ./config.sh remove --unattended --token ${ACCESS_TOKEN}
+    ./config.sh remove --token ${ACCESS_TOKEN}
 }
 
 trap 'cleanup; exit 130' INT
